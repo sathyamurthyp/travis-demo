cc04acf28dc5f45c50f4451e190cacc89429ed88
sathyamurthyp <sathyamurthyp@users.noreply.github.com>
2016-09-02 16:21:38 +0530

    Update EnglishGreeter.java

--- src/main/java/org/markjszy/model/EnglishGreeter.java
+++ src/main/java/org/markjszy/model/EnglishGreeter.java
@@ -5,7 +5,7 @@ package org.markjszy.model;
  */
 public class EnglishGreeter {
     public static String greet(Person p) {
-        return "Hello, " + p.getFirstName() + "!";
+        return "Hai, " + p.getFirstName() + "!";
     }
     
     public static String morningGreet(Person p) {
